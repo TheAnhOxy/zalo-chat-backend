34bf04e4f79db04e25ca59eedaccf014f48d58dc
Thế Anh Oxy <139890192+TheAnhOxy@users.noreply.github.com>
2026-04-09 22:51:11 +0700

    feat: config exception

---
+++ src/main/java/com/zalo/exception/ForBiddenException.java
@@ -0,0 +1,8 @@
+package com.zalo.exception;
+
+
+public class ForBiddenException extends RuntimeException {
+    public ForBiddenException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/zalo/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.zalo.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
